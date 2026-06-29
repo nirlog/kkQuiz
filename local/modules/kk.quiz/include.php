1c510d1a5e3206c34be0bc8bc23269f5a94afb82
nirlog <nirlog@yandex.ru>
2026-06-29 15:08:52 +0300

    Add KK Answers iblock property type

---
+++ local/modules/kk.quiz/include.php
@@ -0,0 +1,16 @@
+<?php
+
+use Bitrix\Main\Loader;
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+Loader::registerAutoLoadClasses(
+    'kk.quiz',
+    [
+        'Kk\\Quiz\\Helper\\Module' => 'lib/Helper/Module.php',
+        'Kk\\Quiz\\Iblock\\Installer' => 'lib/Iblock/Installer.php',
+        'Kk\\Quiz\\Iblock\\Property\\QuizAnswersProperty' => 'lib/Iblock/Property/QuizAnswersProperty.php',
+    ]
+);
