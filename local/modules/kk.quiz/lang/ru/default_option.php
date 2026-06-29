64c5d03c68737d182f52b99df3558bd5f3c0ab53
nirlog <nirlog@yandex.ru>
2026-06-29 14:56:43 +0300

    Prepare KK Answers custom property groundwork

---
+++ local/modules/kk.quiz/lang/ru/default_option.php
@@ -0,0 +1,5 @@
+<?php
+
+$MESS['KK_QUIZ_DEFAULT_EMAIL_FROM'] = 'Email отправителя';
+$MESS['KK_QUIZ_DEFAULT_EMAIL_TO'] = 'Email получателя';
+$MESS['KK_QUIZ_DEFAULT_USE_METRIKA'] = 'Использовать Яндекс.Метрику';
