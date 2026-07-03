46f99dacaeda2ad6f1568a6454403346d3b228a9
nirlog <nirlog@yandex.ru>
2026-07-03 12:28:09 +0300

    Support Telegram topic test sends

--- local/modules/kk.quiz/include.php
+++ local/modules/kk.quiz/include.php
@@ -14,6 +14,11 @@ Loader::registerAutoLoadClasses(
         'Kk\\Quiz\\Iblock\\Property\\QuizAnswersProperty' => 'lib/Iblock/Property/QuizAnswersProperty.php',
         'Kk\\Quiz\\Admin\\ElementFormAssets' => 'lib/Admin/ElementFormAssets.php',
         'Kk\\Quiz\\Repository\\QuizRepository' => 'lib/Repository/QuizRepository.php',
+        'Kk\\Quiz\\Repository\\LeadRepository' => 'lib/Repository/LeadRepository.php',
         'Kk\\Quiz\\Service\\QuizService' => 'lib/Service/QuizService.php',
+        'Kk\\Quiz\\Service\\LeadService' => 'lib/Service/LeadService.php',
+        'Kk\\Quiz\\Service\\ModuleSettingsService' => 'lib/Service/ModuleSettingsService.php',
+        'Kk\\Quiz\\Service\\TelegramNotificationService' => 'lib/Service/TelegramNotificationService.php',
+        'Kk\\Quiz\\Controller\\Api' => 'lib/Controller/Api.php',
     ]
 );
