f3e8761ef746dc39e9e6050230878ef3bd627107
nirlog <nirlog@yandex.ru>
2026-07-28 18:49:12 +0300

    Add dedicated lead detail admin page

--- local/modules/kk.quiz/admin/menu.php
+++ local/modules/kk.quiz/admin/menu.php
@@ -9,17 +9,32 @@ if (!is_object($USER) || !$USER->IsAuthorized() || !$USER->IsAdmin()) {
 }
 
 return [
-    'parent_menu' => 'global_menu_services',
+    'parent_menu' => 'global_menu_kk_quiz',
     'section' => 'kk_quiz',
-    'sort' => 500,
+    'sort' => 100,
     'text' => 'KK Quiz',
     'title' => 'KK Quiz',
     'items_id' => 'menu_kk_quiz',
     'items' => [
         [
+            'text' => 'Квизы',
+            'url' => 'kk_quiz_quizzes.php?lang=' . (defined('LANGUAGE_ID') ? LANGUAGE_ID : 'ru'),
+            'title' => 'Список квизов',
+        ],
+        [
+            'text' => 'Заявки',
+            'url' => 'kk_quiz_leads.php?lang=' . (defined('LANGUAGE_ID') ? LANGUAGE_ID : 'ru'),
+            'title' => 'Заявки квизов',
+        ],
+        [
             'text' => 'Статистика',
             'url' => 'kk_quiz_statistics.php?lang=' . (defined('LANGUAGE_ID') ? LANGUAGE_ID : 'ru'),
             'title' => 'Статистика квизов',
+        ],
+        [
+            'text' => 'Настройки',
+            'url' => 'settings.php?mid=kk.quiz&lang=' . (defined('LANGUAGE_ID') ? LANGUAGE_ID : 'ru'),
+            'title' => 'Настройки модуля KK Quiz',
         ],
         [
             'text' => 'Помощь',
